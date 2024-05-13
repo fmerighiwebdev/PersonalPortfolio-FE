2e78a286771c265f5967d82a81c494df1449fe77
francescomerighi1202 <francescomerighi61@gmail.com>
2024-05-13 11:24:03 +0200

    Bug fix e rimozione pagina Not Found

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AOS from 'aos';
 
 import "./styles/App.css";
@@ -7,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
 
 import Home from "./components/Home";
-import NotFound from "./components/NotFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -20,19 +18,11 @@ function App() {
   });
 
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={
-            <>
-              <Header isLightMode={isLightMode} setIsLightMode={setIsLightMode} />
-              <Home isLightMode={isLightMode} />
-              <Footer isLightMode={isLightMode} />
-            </>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <Header isLightMode={isLightMode} setIsLightMode={setIsLightMode} />
+      <Home isLightMode={isLightMode} />
+      <Footer isLightMode={isLightMode} />
+    </>
   );
 }
 
